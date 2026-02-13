f443e2629510fccb1d2528e118c5fcc238c726a3
KUNAL PRAMANIK <pramanikkunal65@gmail.com>
2026-02-14 03:56:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import threading
 
 # 🔒 SECURE: Must be set in Render Environment Variables
 HF_TOKEN = os.environ.get("HF_TOKEN") 
-API_URL = "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2"
+API_URL = "https://router.huggingface.co/hf-inference/models/sentence-transformers/all-MiniLM-L6-v2/pipeline/feature-extraction"
 # Added User-Agent to prevent potential API blocks
 headers = {
     "Authorization": f"Bearer {HF_TOKEN}",
